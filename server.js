a1f128b4865a159da1e1401cd9460ccb3c750049
Phamtien <phamtien1709@gmail.com>
2017-08-30 11:38:21 +0700

    Homework 27/8

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname + '/css'));
+app.use('/css', express.static('css'));
+app.use('/img', express.static('img'));
+app.use('/js', express.static('js'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname = '/index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname = '/public/cvstrap.html')
+  res.sendFile(__dirname + '/public/cvstrap.html')
 });
 
 app.get('/read', (req, res) => {
-  res.sendFile(__dirname = '/public/readfile.html')
+  res.sendFile(__dirname + '/public/readfile.html')
 });
 
 app.listen(6969, () => {
