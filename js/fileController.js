b1eb1f3c7f75b19065a14c412ea25d8d2e7c817c
Phamtien <phamtien1709@gmail.com>
2017-09-06 03:04:12 +0700

    asdnjksan

--- js/fileController.js
+++ js/fileController.js
@@ -1,14 +1,36 @@
 const fs = require('fs');
 
-const saveFile = (filename, data) => {
+const saveFileSync = (filename, data) => {
   fs.writeFileSync(filename, data);
 };
 
-const readFile = (filename) => {
+const readFileSync = (filename) => {
   return fs.readFileSync(filename, { encoding : 'utf-8'});
 };
 
+const appendFileSync = (filename, data) => {
+  return fs.appendFileSync(filename, data," utf-8 ");
+};
+
+const getListQuestion = () => {
+  let questionToString = `[${readFileSync('./question.txt')}]`;
+  try {
+    listQuestion = JSON.parse(questionToString);
+    return listQuestion;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
+
+const getTotalQuestion = () => {
+  return getListQuestion().length;
+}
+
 module.exports = {
-  saveFile,
-  readFile
+  saveFileSync,
+  readFileSync,
+  appendFileSync,
+  getListQuestion,
+  getTotalQuestion
 };
--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-Parser');
+const questionRouter = require('./router/questionRouter');
+const askRouter = require('./router/askRouter');
+const readFileRouter = require('./router/readFileRouter');
+
+
 
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.use('/css', express.static('css'));
 app.use('/img', express.static('img'));
 app.use('/js', express.static('js'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// get Router
+app.use('/', askRouter);
+app.use('/question', questionRouter);
+app.use('/read', readFileRouter);
 
-app.get('/home', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
+//render handlebars
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/public/cvstrap.html')
+  res.render('about');
 });
 
-app.get('/read', (req, res) => {
-  res.sendFile(__dirname + '/public/readfile.html')
-});
 
 app.listen(6969, () => {
   console.log('server is up');
